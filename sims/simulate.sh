5b252105613a804d6f9311e4bc877e94d89c4d6c
shyamsg <shyam.g@gmail.com>
2014-06-04 00:57:43 -0500

    Changed simulation code

--- sims/simulate.sh
+++ sims/simulate.sh
@@ -14,12 +14,23 @@ n1=1000
 n2=1000
 
 ## Scenario 1: Te=8KYA, Ts=2,4,6,8 KYA m=0.001,0.0001,0.00001
+# Migration rate 0.00001
 # Ts=2Kya Te=8KYA, 8KYA=320gens=0.00008 in 4N0 generations 2KYA=0.00002
-macs $sample $region -t $theta -r $rho -T -I 2 $n1 $n2 40 -eM 0.00002 0 -ej 0.00002001 2 1 -en 0.00002002 2 0 -en 0.00008 1 0.01 > sim.ts2k.te8k.out 
+macs $sample $region -t $theta -r $rho -T -I 2 $n1 $n2 40 -eM 0.00002 0 -ej 0.00002001 2 1 -en 0.00002002 2 0 -en 0.00008 1 0.01 1> sim.m5.ts2k.te8k.out 2> /dev/null
 
 # Ts=4Kya Te=8KYA, 8KYA=320gens=0.00008 in 4N0 generations 4KYA=0.00004
-macs $sample $region -t $theta -r $rho -T -I 2 $n1 $n2 40 -eM 0.00004 0 -ej 0.00004001 2 1 -eN 0.00004002 2 0 -en 0.00008 1 0.01 > sim.ts4k.t28k.out
+macs $sample $region -t $theta -r $rho -T -I 2 $n1 $n2 40 -eM 0.00004 0 -ej 0.00004001 2 1 -eN 0.00004002 2 0 -en 0.00008 1 0.01 1> sim.m5.ts4k.t28k.out 2> /dev/null
 
 # Ts=6Kya Te=8KYA, 8KYA=320gens=0.00008 in 4N0 generations 6KYA=0.00006
-macs $sample $region -t $theta -r $rho -T -I 2 $n1 $n2 40 -eM 0.00006 0 -ej 0.00006001 2 1 -eN 0.00006002 2 0 -en 0.00008 1 0.01 > sim.ts6k.te8k.out
+macs $sample $region -t $theta -r $rho -T -I 2 $n1 $n2 40 -eM 0.00006 0 -ej 0.00006001 2 1 -eN 0.00006002 2 0 -en 0.00008 1 0.01 1> sim.m5.ts6k.te8k.out 2> /dev/null
+
+# Migration rate 0.000001
+# Ts=2Kya Te=8KYA, 8KYA=320gens=0.00008 in 4N0 generations 2KYA=0.00002
+macs $sample $region -t $theta -r $rho -T -I 2 $n1 $n2 4 -eM 0.00002 0 -ej 0.00002001 2 1 -en 0.00002002 2 0 -en 0.00008 1 0.01 1> sim.m6.ts2k.te8k.out 2> /dev/null
+
+# Ts=4Kya Te=8KYA, 8KYA=320gens=0.00008 in 4N0 generations 4KYA=0.00004
+macs $sample $region -t $theta -r $rho -T -I 2 $n1 $n2 4 -eM 0.00004 0 -ej 0.00004001 2 1 -eN 0.00004002 2 0 -en 0.00008 1 0.01 1> sim.m6.ts4k.t28k.out 2> /dev/null
+
+# Ts=6Kya Te=8KYA, 8KYA=320gens=0.00008 in 4N0 generations 6KYA=0.00006
+macs $sample $region -t $theta -r $rho -T -I 2 $n1 $n2 4 -eM 0.00006 0 -ej 0.00006001 2 1 -eN 0.00006002 2 0 -en 0.00008 1 0.01 1> sim.m6.ts6k.te8k.out 2> /dev/null
 
